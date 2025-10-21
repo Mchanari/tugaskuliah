73a86bafc8f2949d06a4e252d4c0284a98c49519
hanari <irfanmaulanahakim04@gmail.com>
2025-10-21 13:27:54 +0700

    Tugas logika informatika foward chaining.py

--- Tugas logika informatika foward chaining.py
+++ Tugas logika informatika foward chaining.py
@@ -1,24 +1,15 @@
-# ======== Forward Chaining untuk Diagnosis Komputer ========
+rules = [ (["komputer_tidak_nyala", "tombol_power_berfungsi"], "cek_suplai_daya"), (["cek_suplai_daya", "kabel_terpasang_baik"], "kemungkinan_psu_rusak"),
+    (["komputer_tidak_nyala", "ada_beep"], "masalah_ram"), (["komputer_nyala_tapi_tidak_tampil", "monitor_nyala"], "masalah_vga"),
+    (["masalah_ram", "ram_terpasang_baik"], "bersihkan_kontak_ram"), (["masalah_vga", "vga_terintegrasi"], "update_driver") ]
 
-rules = [
-    (["komputer_tidak_nyala", "tombol_power_berfungsi"], "cek_suplai_daya"),
-    (["cek_suplai_daya", "kabel_terpasang_baik"], "kemungkinan_psu_rusak"),
-    (["komputer_tidak_nyala", "ada_beep"], "masalah_ram"),
-    (["komputer_nyala_tapi_tidak_tampil", "monitor_nyala"], "masalah_vga"),
-    (["masalah_ram", "ram_terpasang_baik"], "bersihkan_kontak_ram"),
-    (["masalah_vga", "vga_terintegrasi"], "update_driver")
-]
-
-facts = {
-    "komputer_tidak_nyala": True,
+facts = {"komputer_tidak_nyala": True,
     "tombol_power_berfungsi": True,
     "kabel_terpasang_baik": True,
     "ada_beep": False,
     "komputer_nyala_tapi_tidak_tampil": False,
     "monitor_nyala": False,
     "ram_terpasang_baik": True,
-    "vga_terintegrasi": True
-}
+    "vga_terintegrasi": True }
 
 def forward_chaining(rules, facts):
     inferred = set()
@@ -36,4 +27,5 @@ def forward_chaining(rules, facts):
 
 print("=== FORWARD CHAINING ===")
 hasil_fc = forward_chaining(rules, facts)
+
 print("\nFakta akhir:", facts)
